7210b5e9aa2d36f03704286b2961935ab49ab977
Felipe Avelar <30672253+FelipeFMA@users.noreply.github.com>
2024-07-05 20:55:55 -0300

    added the packages script

---
+++ packages.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Script to convert the result of 'pacman -Qe' into a one-line list of packages without the version numbers.
+# By: Felipe Avelar.
+
+
+# check for user error
+if [ -z "$1" ]; then
+  echo "Usage: $0 packages.txt"
+  exit 1
+fi
+
+# verify if file exists
+if [ ! -f "$1" ]; then
+  echo "file not found: $1"
+  exit 1
+fi
+
+# remove number version
+result=""
+while IFS= read -r line; do
+  package=$(echo "$line" | awk '{print $1}')
+  result+="$package "
+done < "$1"
+
+# make it one line
+result=$(echo "$result" | sed 's/ *$//')
+
+# print the result
+echo "$result"
