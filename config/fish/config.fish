1bc10d3b80392bb07e1442ee32a039297b2aab48
FelipeFMA <felipefmavelar@gmail.com>
2025-04-25 18:26:39 -0300

    updated my configs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
     sh -c echo
-    sh -c 'fastfetch -l small'
+    sh -c 'figlet "Arch BTW"'
     sh -c echo
     sh -c /home/felipe/Documents/Github/ISORememberer/rememberer.sh
 end
